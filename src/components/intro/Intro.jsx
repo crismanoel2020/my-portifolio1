3a619a5b76ce2055b344b08ff998323d43f439c6
crismanoel2020 <cristianofront2018@gmail.com>
2022-01-25 21:04:24 -0300

    Subindo as alteracoes do efeitos de transicao de nome e animacoes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,18 @@
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
 
-export default function Intro (){
+export default function Intro () {
+
+    const textRef = useRef();
+
+    useEffect(()=>{
+      init(textRef.current, { 
+          showCursor: false, 
+          strings: ['Developer', 'Designer','Criador de Conteudo!' ] })
+    }, []);
+
+
     return (
         <div className="intro" id="intro">
            <div className="left">
@@ -9,7 +21,16 @@ export default function Intro (){
                     <img src="assets/man.png" alt="" />
                 </div>
            </div>
-           <div className="right"></div>
+           <div className="right">
+               <div className="wrapper">
+                   <h1>Olá, eu sou..</h1>
+                   <h2>Cristiano Manoel Da Silva</h2>
+                   <h3>Freelance <span ref={textRef}>Designer</span></h3>
+               </div>
+               <a href="#portifolio">
+                   <img src="assets/selecionado.png" alt="" />
+               </a>
+           </div>
         </div>
     )
 }
